50dd87a683d807bdccf291270742138074c3f8a4
Yanmin Li <li.yanmin@rytong.com>
2016-11-01 15:24:02 +0800

    二次修改提交测试

--- TestApp/TestApp/ViewController.m
+++ TestApp/TestApp/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello world");
+    NSLog(@"ceshi");
 }
 
 - (void)didReceiveMemoryWarning {
