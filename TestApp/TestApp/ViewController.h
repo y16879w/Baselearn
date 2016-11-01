83876c30b4c1d967713f3484416745de18361bab
Yanmin Li <li.yanmin@rytong.com>
2016-11-01 14:58:03 +0800

    第一个测试应用

---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Ryt on 16/11/1.
+//  Copyright © 2016年 Liyanmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by Ryt on 16/11/1.
+//  Copyright © 2016年 Liyanmin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hello world");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
